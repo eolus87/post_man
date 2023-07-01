63e394bcfc91b9447eae3a5105ea84b7ead7a732
eolus87 <niguvaz@gmail.com>
2023-07-02 00:27:54 +0100

    Connections are now created and close every time an interaction with the DB is made.

--- interfaces/postgresql.py
+++ interfaces/postgresql.py
@@ -1,13 +1,14 @@
 __author__ = "Nicolas Gutierrez"
 
 # Standard Libraries
+import logging
+# Third party libraries
 import psycopg2
 from psycopg2.extensions import connection
-# Third party libraries
 # Custom Libraries
 from data_classes.data_point import DataPoint
 
-creation_command = (
+table_creation_query = (
     """
     CREATE TABLE IF NOT EXISTS {0}(
             time_stamp TIMESTAMP,
@@ -19,46 +20,104 @@ creation_command = (
     )
     """)
 
-insertion_command = (
+data_insertion_query = (
         """INSERT INTO {0}(time_stamp, type, target, value, unit) 
         VALUES('{1}', '{2}', '{3}', {4}, '{5}')
         """)
 
 
 class PostGreSQL:
-    def __init__(self, ip: str, port: int, user: str, password: str, table_name: str) -> None:
+    def __init__(
+            self,
+            address: str,
+            port: int,
+            user: str,
+            password: str,
+            table_name: str
+    ) -> None:
+        # Logger
+        self.__logger = logging.getLogger(f"post_man.{__name__}")
         # Inputs
-        self.table_name = table_name
-        # Connection to the db
-        self.conn = self.__connect_to_db(ip, port, user, password)
+        self.__address = address
+        self.__port = port
+        self.__user = user
+        self.__password = password
+        self.__table_name = table_name
         # Init table
         self.__init_table(table_name)
 
-    @staticmethod
-    def __connect_to_db(ip: str, port: int, user: str, password: str) -> connection:
-        conn = psycopg2.connect(host=ip, port=port, user=user, password=password)
+    def __connect_to_db(self) -> connection:
+        """
+        Function to connect to the PostGreSQL DB using the package psycopg2.
+        It sets up autocommit as True automatically.
+
+        :return: It returns the DB connection
+        """
+        conn = psycopg2.connect(
+            host=self.__address,
+            port=self.__port,
+            user=self.__user,
+            password=self.__password
+        )
         conn.autocommit = True
         return conn
 
     def __init_table(self, table_name: str) -> None:
-        table_creation_command = creation_command.format(table_name)
-        self.__execute_cursor(table_creation_command)
+        """
+        Initializes the table in the DB. This is always done when this class
+        is instantiated. It is recommended the query includes a condition
+        in case the table exists.
 
-    def __execute_cursor(self, command: str) -> None:
-        cur = self.conn.cursor()
+        :param table_name: String with the name of the table
+        """
+        conn = self.__connect_to_db()
+        try:
+            table_creation_command = table_creation_query.format(table_name)
+            self.__execute_cursor(conn, table_creation_command)
+        except Exception as inst:
+            self.__logger.error(f"Error while creating DB table: {inst}")
+        finally:
+            self.__close_connection(conn)
+
+    @staticmethod
+    def __execute_cursor(conn: connection, command: str) -> None:
+        """
+        Auxiliary methods that instantiates a cursor, executes and close it.
+
+        :param command: String with the query to execute
+        """
+        cur = conn.cursor()
         cur.execute(command)
         cur.close()
 
     def insert_data(self, data_point: DataPoint) -> None:
-        data_insertion_command = insertion_command.format(
-            self.table_name,
-            data_point.time_stamp,
-            data_point.type,
-            data_point.target,
-            data_point.value,
-            data_point.unit
-        )
-        self.__execute_cursor(data_insertion_command)
+        """
+        Method that inserts the data into the DB. It sets up the query
+        message using a data_class data_point and executes the query
+        of insertion.
 
-    def close_connection(self) -> None:
-        self.conn.close()
+        :param data_point: DataPoint dataclass with the information
+        of the sensor
+        """
+        conn = self.__connect_to_db()
+        try:
+            data_insertion_command = data_insertion_query.format(
+                self.__table_name,
+                data_point.time_stamp,
+                data_point.type,
+                data_point.target,
+                data_point.value,
+                data_point.unit
+            )
+            self.__execute_cursor(conn, data_insertion_command)
+        except Exception as inst:
+            self.__logger.error(f"Error while inserting data into DB: {inst}")
+        finally:
+            self.__close_connection(conn)
+
+    @staticmethod
+    def __close_connection(conn: connection) -> None:
+        """
+        Wrapper to close connection.
+        """
+        conn.close()
