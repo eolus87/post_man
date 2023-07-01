edcbdad953da3fd54f135a9cfcd837aa74c44ac3
eolus87 <niguvaz@gmail.com>
2023-07-02 00:27:25 +0100

    Included the docstring into the data point data class

--- data_classes/data_point.py
+++ data_classes/data_point.py
@@ -10,7 +10,15 @@ import datetime
 @dataclass(frozen=True)
 class DataPoint:
     """
-    Data Class to store the data measured by a single sensor in a single point in time.
+    Data Class to store the data measured by a single sensor
+    in a single point in time. It is frozen, so it will error
+    if any method tries to modify the data on it.
+
+    :param time_stamp: Time in format datetime of the data sampled
+    :param type: String with the type of sensor, e.g. "power", "ping"...
+    :param target: String with the ip of the sensor
+    :param value: Data value extracted from the sensor
+    :param unit: Unit of the value extracted from the sensor
     """
 
     time_stamp: datetime.datetime = field(init=True, repr=True)
