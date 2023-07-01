8dbdd99892706873ada08f337c4241eae18a08b2
eolus87 <niguvaz@gmail.com>
2023-07-02 00:40:05 +0100

    Corrected a bug in the power function

--- power/power_function.py
+++ power/power_function.py
@@ -3,7 +3,7 @@ __author__ = "Nicolas Gutierrez"
 # Standard libraries
 import platform
 import asyncio
-from typing import List, Tuple, Union
+from typing import List, Union
 # Third party libraries
 import kasa
 # Custom libraries
@@ -12,14 +12,14 @@ if platform.system() == 'Windows':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 
-async def power_request(target: str) -> List[List[Union[str, float]]]:
+async def power_request(target: str) -> float:
     plug = kasa.SmartPlug(target)
     await plug.update()
     power = plug.emeter_realtime.power
     del plug
+    return power
+
+
+def power_function(target: str) -> List[List[Union[float, str]]]:
+    power = asyncio.run(power_request(target))
     return [["power", power, "W"]]
-
-
-def power_function(target: str) -> Tuple[float, str]:
-    power, unit = asyncio.run(power_request(target))
-    return power, unit
