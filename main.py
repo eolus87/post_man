1487cbb60b4700cbf863ba954cf48695ec8c1ba8
eolus87 <niguvaz@gmail.com>
2023-07-02 00:28:51 +0100

    Adapted the main to avoid closing the connection to the DB and including the new wifi_iot_leader

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ __author__ = "Nicolas Gutierrez"
 # Standard libraries
 import os
 import time
-# Custom libraries
 # Third party libraries
+# Custom libraries
 from utilities.utilities import load_yaml
 from labourers.leader import Leader
 from network.ping_function import ping_function
 from power.power_function import power_function
+from wifi_iot.wifi_iot_function import wifi_iot_request
 from interfaces.postgresql import PostGreSQL
 from utilities.init_logger import init_logger
 
 logger = init_logger("post_man_logs.txt")
 
 logger.info("Reading configuration")
-configuration_path = os.path.join("config", "config.yaml")
-configuration = load_yaml(configuration_path)
+try:
+    configuration_path = os.path.join("config", "config.yaml")
+    configuration = load_yaml(configuration_path)
+except Exception as inst:
+    logger.error(f"Error while loading configuration: {inst}")
+    raise Exception(inst)
 
 logger.info("Initializing PostGreSQL")
 postgre_interfacer = PostGreSQL(
@@ -30,6 +35,7 @@ postgre_interfacer = PostGreSQL(
 logger.info("Initializing leaders")
 ping_leader = Leader(configuration["network"], ping_function, postgre_interfacer.insert_data)
 power_leader = Leader(configuration["power"], power_function, postgre_interfacer.insert_data)
+wifi_iot_leader = Leader(configuration["iot"], wifi_iot_request, postgre_interfacer.insert_data)
 try:
     logger.info("Starting leaders")
     ping_leader.start()
@@ -45,6 +51,4 @@ logger.info("Stopping Leaders")
 ping_leader.stop()
 power_leader.stop()
 logger.info("Stopped Leaders")
-postgre_interfacer.close_connection()
-logger.info("Stopped interface")
 logger.info("Exiting code")
