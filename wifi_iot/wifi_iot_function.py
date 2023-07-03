2fe0083291c4bd20ceea5495d9a67be1d2190450
eolus87 <niguvaz@gmail.com>
2023-07-03 21:20:34 +0100

    Included a timeout for the website

--- wifi_iot/wifi_iot_function.py
+++ wifi_iot/wifi_iot_function.py
@@ -7,19 +7,22 @@ from typing import List, Union
 from bs4 import BeautifulSoup
 # Custom libraries
 
+REQUEST_TIMEOUT = 2
+
 
 def wifi_iot_request(target: str) -> List[List[Union[str, float]]]:
     """
-    Parser of the wifi_iot_sensor/s from html code. Sensor info should be included
+    Parser of the wifi_iot_sensor/s from response code. Sensor info should be included
     as h3 header level and contain the following format:
     <h3>Sensor_Type Sensor_value Sensor_unit<\h3>
 
-    :param target: ip of the sensor with html web interface
+    :param target: ip of the sensor with response web interface
     :return: list with as many lists as sensors and 3 components correctly
     formatted as type_of_sensor, value and unit.
     """
-    html = urlopen(f"http://{target}").read()
-    soup = BeautifulSoup(html, features="html.parser")
+
+    response = urlopen(f"http://{target}", timeout=REQUEST_TIMEOUT).read()
+    soup = BeautifulSoup(response, features="html.parser")
     sensors = soup.find_all("h3")
 
     list_of_sensors = []
