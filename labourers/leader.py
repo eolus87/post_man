a0aa0d9a42228ca77c25a451f7b0729854ea72eb
eolus87 <niguvaz@gmail.com>
2023-07-18 21:11:58 +0100

    Reformatted the file to reduce line length and improved logging.

--- labourers/leader.py
+++ labourers/leader.py
@@ -13,22 +13,38 @@ from labourers.worker import Worker
 
 
 class Leader:
-    def __init__(self, leader_config: Dict, work_function: Callable, publish_function: Callable) -> None:
+    def __init__(
+            self,
+            leader_config: Dict,
+            work_function: Callable,
+            publish_function: Callable
+    ) -> None:
         self.__logger = logging.getLogger(f"post_man.{__name__}")
 
         # Initialization of components
         self.__measures_queue = Queue(maxsize=len(leader_config["devices"])*10)
-        self.__workers = self.__workers_instantiation(leader_config, self.__measures_queue, work_function)
+        self.__workers = self.__workers_instantiation(
+            leader_config,
+            self.__measures_queue,
+            work_function
+        )
 
         # Configuration of threads
         self.started_event = threading.Event()
         self.__management_thread_loop_sleep = 0.25
         self.__management_thread_exit_sleep = 0.25
         self.__keep_managing = True
-        self.__management_thread = Thread(target=self.__leader_management, args=[publish_function])
+        self.__management_thread = Thread(
+            target=self.__leader_management,
+            args=[publish_function]
+        )
 
     @staticmethod
-    def __workers_instantiation(config: Dict, data_queue: Queue, work_function: Callable) -> List[Worker]:
+    def __workers_instantiation(
+            config: Dict,
+            data_queue: Queue,
+            work_function: Callable
+    ) -> List[Worker]:
         list_of_threads = []
         # Initialization of threads
         for target_key in config["devices"].keys():
@@ -48,7 +64,9 @@ class Leader:
                 try:
                     publish_function(self.__measures_queue.get())
                 except Exception as inst:
-                    self.__logger.error(f"Error while publishing using: {inst}")
+                    self.__logger.error(
+                        f"Error while publishing using publish function: {inst}"
+                    )
             time.sleep(self.__management_thread_loop_sleep)
 
     def start(self) -> None:
